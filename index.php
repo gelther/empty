758cbeae953117d61c93c1f8e6d5e60c596c60f7
gelther <lpw.sormano@gmail.com>
2016-04-16 14:36:04 +0200

    Update and rename index to index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    
+echo "hello wolrd"
+?>
+<div class="">Boo!</div>
+
+<?php 
+
+/**
+ *  Hello world function.
+ */
+ 
+function hello_world ( $a,$b)
+{
+  echo $a;
+  return $b;
+}
+
+?>
