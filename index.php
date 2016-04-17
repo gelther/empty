3e9caae3d3a25002d9c82f2359e4ca6b0ae8c83e
gelther <lpw.sormano@gmail.com>
2016-04-17 23:06:29 +0200

    Code consistency changes for index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-    
-echo "hello wolrd"
+
+echo "hello wolrd";
 ?>
 <div class="">Boo!</div>
 
-<?php 
+<?php
 
-/**  
+/**
  *  Hello world function.
  */
-    
-function hello_world ( $a,$b)  
-{ 
+
+function hello_world ( $a,$b)
+{
   echo $a;
-  return $b;  
+  return $b;
 }
 
 ?>
